2ee1ecb2f3f80bae70dec06167ab52d46037e608
JohnnyParra <juanparra1996@gmail.com>
2023-02-14 16:08:06 -0800

    structure for topPost.js command  complete

--- src/commands/tools/topPost.js
+++ src/commands/tools/topPost.js
@@ -5,23 +5,58 @@ module.exports = {
     .setName("top-post")
     .setDescription("Returns top post on this channel"),
   async execute(interaction, client) {
-    let reactionsArray = []
 
-    const messages = await client.channels.cache
-      .get(interaction.channel.id)
-      .messages.fetch({ limit: 100 });
+    async function getMessages(){
+      const messages = await client.channels.cache
+        .get(interaction.channel.id)
+        .messages.fetch({ limit: 100 })
+      return messages
+    }
 
-    const reactions = await messages.forEach(message => {
-      message.reactions.cache.forEach(reaction => {
-        reactionsArray.push({count: reaction.count, id: reaction.message.id})
+    async function getReaction(){
+      let reactionsArray = []
+      const messages = await getMessages();
+      await messages.forEach(message => {
+        // console.log(message.reactions.cache);
+        message.reactions.cache.forEach(reaction => {
+          const index = reactionsArray.findIndex(e => e.id === reaction.message.id)
+          if (index > -1){
+            reactionsArray[index].count += reaction.count;
+          } else{
+            reactionsArray.push({count: reaction.count, id: reaction.message.id})
+          }
+        })
       })
-    });
-    console.log(reactionsArray);
-    // const embed = new EmbedBuilder()
-    //   .setTitle("Top Posts")
+      return reactionsArray.sort((x,y) => y.count - x.count).splice(0,3);
+    }
 
-    // await interaction.reply({
-    //   embeds: [embed],
-    // });
+    const reactionsArray = await getReaction();
+
+    const embed = new EmbedBuilder()
+    .setTitle(`Top Posts`)
+    .setDescription("Top 3 posts from this channel")
+    .setColor(0x12e1ee)
+    .setTimestamp(Date.now())
+    .addFields([
+      {
+        name: "#1",
+        value: `${reactionsArray[0].count}`,
+        inline: true,
+      },
+      {
+        name: "#2",
+        value: `${reactionsArray[1].count}`,
+        inline: true,
+      },
+      {
+        name: "#3",
+        value: `${reactionsArray[2].count}`,
+        inline: true,
+      },
+    ]);
+
+    await interaction.reply({
+      embeds: [embed],
+    });
   },
 };
