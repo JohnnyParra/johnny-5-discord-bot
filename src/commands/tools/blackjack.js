209db541a89b131527b6b0a4f2d886e675e1cead
JohnnyParra <juanparra1996@gmail.com>
2023-02-21 16:51:33 -0800

    added buttons and deck to blackjack command

--- src/commands/tools/blackjack.js
+++ src/commands/tools/blackjack.js
@@ -1,13 +1,48 @@
-const { SlashCommandBuilder } = require("discord.js");
+const { Deck, shuffle } = require("../../utils/deck");
+const { SlashCommandBuilder, ButtonBuilder, ActionRowBuilder, ButtonStyle, MessageCollector } = require("discord.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("blackjack")
     .setDescription("play blackjack"),
   async execute(interaction) {
+    let deck = shuffle(Deck);
+    let dealer = [deck[1], deck[3]];
+    let player = [deck[0], deck[2]];
+    console.log(dealer, player);
+
+    const row = new ActionRowBuilder()
+      .addComponents(
+        new ButtonBuilder()
+          .setCustomId('hit')
+          .setLabel('Hit')
+          .setStyle(ButtonStyle.Primary),
+        new ButtonBuilder()
+          .setCustomId('stand')
+          .setLabel('Stand')
+          .setStyle(ButtonStyle.Danger),
+      )
 
     await interaction.reply({
       content: "play a game of blackjack",
+      components: [row]
     });
+
+    const collector = interaction.channel.createMessageComponentCollector({
+      max: 1,
+      time: 1000 * 15,
+    });
+
+    collector.on('collect', () => {
+      interaction.editReply({
+        content: 'you clicked the button'
+      })
+    });
+
+    collector.on('end', (collection) => {
+      collection.forEach((click) => {
+        console.log(click.customId);
+      })
+    })
   },
 };
